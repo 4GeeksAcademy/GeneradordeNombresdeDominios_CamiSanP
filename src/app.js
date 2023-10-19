ee036ce60a278a3aa28df98523c608002c1a4dfb
CamiSanP <145134410+CamiSanP@users.noreply.github.com>
2023-10-19 00:00:04 +0000

    [GeneradorNdeDom]

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronouns = ['the', 'our'];
+let adjetives = ['great', 'big'];
+let nouns = ['jogger', 'racoon','internet', 'lastofus','puedes'];
+let extensions =['.com','.net','.io','.us', '.es'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const specialDomains = {
+  "internet": ".net",
+  "puedes": ".es",
+  "lastofus": ".us"
+}
+function generateDomainNames (){
+  let domainNames = []
+  for(let pronoun of pronouns){
+    for(let adjetive of adjetives){
+      for(let noun of nouns){
+        for(let extension of extensions){
+          let newDomain = ""
+          // console.log(pronoun[items]+adj[obj]+noun[item]+type[obs])
+          if (noun in specialDomains && specialDomains [noun] === extension){
+            // domainNames.push (`${noun.slice(0,-extension.length + 1)}${extension}`) 
+            newDomain = `${noun.slice(0,-extension.length + 1)}${extension}`
+          } else {
+            // domainNames.push (`${pronoun}${adjetive}${noun}${extension}`)
+            newDomain = `${pronoun}${adjetive}${noun}${extension}`
+          }
+          if (!domainNames.includes (newDomain)){
+            domainNames.push(newDomain)
+          }
+        }
+      }
+    }
+  }
+  return domainNames 
+}
+
+
+function printDomainNames (domainNames){
+  for (const domain of domainNames) {
+   console.log(domain) 
+  }
+}
+const domainNames = generateDomainNames()
+printDomainNames(domainNames)
